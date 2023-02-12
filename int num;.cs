d822a0eff6f1004798b9e5947e7208c24a43119b
2019Z2 <79469120+2019Z2@users.noreply.github.com>
2023-02-12 10:37:46 +0000

    lrn rflct 12/2/23

--- int num;.cs
+++ int num;.cs
@@ -1,5 +1,19 @@
-int num;
-console.Write("3nter a number between 1'99");
-num=int.Parse(console.ReadLine());
-if ((num %7==10) || (num/10==7) || (num%10==7))
-console.WriteLine("Boom!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp120123
+{
+    Class Program
+    {
+       int num;
+    Console.Write("enter a number between 1-99");
+    num=int.Parse(Console.ReadLine());
+    if ((num %7==10) || (num/10==7) || (num%10==7))
+    Console.WriteLine("Boom!"); 
+         
+        }
+    }
