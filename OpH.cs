dfad7add7331f3c436ef8871ac5a034c56ad7d66
2019Z2 <79469120+2019Z2@users.noreply.github.com>
2023-03-26 11:49:09 +0300

    Add files via upload
    Reflecting 27-03-23(2)

--- OpH.cs
+++ OpH.cs
@@ -9,13 +9,24 @@ namespace BenDover
       int day;
       Console.WriteLine("Enter a number between 1-7");
       day=int.Parse(Console(ReadLine()));
-      if (day == 1)
-      Console.WriteLine("08:00-18:00");
-      else if
-      (day == 2);
+      switch(day);
       {
-      	Console.WriteLine("10:00-16:30");
-      	Console.WriteLine("18:00-20:30"); }
+      	case 1:
+      	Console.WriteLine("10:00-16:00");
+      	break;
+      	
+      	case 2:
+      	Console.WriteLine("12:00-16:00");
+      	break;
+      	
+      	default:
+      	Console.WriteLine("08:00-15:30");
+      	break;
+     
+      }
+    
+   
+      	
       }
     }
 }
